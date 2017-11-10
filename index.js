581b33f5bbccb448329c099f770e8f9f045e9899
Brendan O'Dea <brendan.ode4@gmail.com>
2017-11-10 23:35:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(string === "I love you, Grandma.") {
-    return "I love you, too." 
-  }   
+    return "I love you, too."
+  }
   elseif (string.toUpperCase() = string) {
     return "YES INDEED!"
   }
