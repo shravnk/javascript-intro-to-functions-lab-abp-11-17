c26ce9a8eada757d819f713a95de0d4cb1bb21ec
Brendan O'Dea <brendan.ode4@gmail.com>
2017-11-10 23:37:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if(string === "I love you, Grandma.") {
     return "I love you, too."
   }
-  elseif (string.toUpperCase() ==== string) {
+  elseif (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
   else {
